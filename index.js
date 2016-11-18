71db915e41c271d17cdb07d5ffb02a95be8b9372
Dianne Laguerta <diannelaguerta@gmail.com>
2016-11-18 14:53:30 -0800

    displaying time without refresh

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
 console.log("make sure this works!");
- });
-// Use the Date Library in Javascript to retrieve the current date and time information. Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
-
 
 // Create a function which will do all of the work to calculate and show the time.
 
@@ -11,17 +8,35 @@ console.log("make sure this works!");
 
 
 // let's make a function!
-//
-// var currentTime = function() {
-//   var time = new Date();
-//   var hours = time.getHours();
-//   var minutes = time.getMinutes();
-//   var seconds = time.getSeconds();
-//   console.log(hours + ":" + minutes + ":" + seconds);
-// };
-//
-// test = currentTime();
+var currentTime = function() {
+  var time = new Date();
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+  return (hours + ":" + minutes + ":" + seconds);
+};
+
+var test = currentTime();
 // console.log(test);
- // Date.prototype.getHours()
-// Date.prototype.getMinutes()
-// Date.prototype.getSeconds()
+
+var clockie = $('<h3>' + test + '</h3>');
+console.log(test);
+
+// var intervalID = window.setInterval(clockie, 10);
+
+// $('#clock').html(clockie);
+
+$('#clock').html(clockie);
+
+ });
+
+
+var display = function() {
+  var test = currentTime();
+  // console.log(test);
+
+  var clockie = $('<h3>' + test + '</h3>');
+
+  return clockie;
+
+};
