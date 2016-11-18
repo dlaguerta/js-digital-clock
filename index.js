f71738ae1e8d1e29138e474de7b3e44f3525be50
Dianne Laguerta <diannelaguerta@gmail.com>
2016-11-18 14:09:14 -0800

    added the function for currentTime

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+$(document).ready(function(){
+// Use the Date Library in Javascript to retrieve the current date and time information. Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
+
+
+// Create a function which will do all of the work to calculate and show the time.
+
+//give you the current date
+// var time = new Date();
+
+
+// let's make a function!
+
+var currentTime = function() {
+  var time = new Date();
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+  console.log(hours + ":" + minutes + ":" + seconds);
+};
+
+test = currentTime();
+console.log(test);
+ // Date.prototype.getHours()
+// Date.prototype.getMinutes()
+// Date.prototype.getSeconds()
