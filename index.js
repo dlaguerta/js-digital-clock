0c33675fc37664e84be92a8bf7d43d2a5e99c4f2
Dianne Laguerta <diannelaguerta@gmail.com>
2016-11-18 14:16:23 -0800

    made sure jquery loaded correctly

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+console.log("make sure this works!");
+ });
 // Use the Date Library in Javascript to retrieve the current date and time information. Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
 
 
@@ -9,17 +11,17 @@ $(document).ready(function(){
 
 
 // let's make a function!
-
-var currentTime = function() {
-  var time = new Date();
-  var hours = time.getHours();
-  var minutes = time.getMinutes();
-  var seconds = time.getSeconds();
-  console.log(hours + ":" + minutes + ":" + seconds);
-};
-
-test = currentTime();
-console.log(test);
+//
+// var currentTime = function() {
+//   var time = new Date();
+//   var hours = time.getHours();
+//   var minutes = time.getMinutes();
+//   var seconds = time.getSeconds();
+//   console.log(hours + ":" + minutes + ":" + seconds);
+// };
+//
+// test = currentTime();
+// console.log(test);
  // Date.prototype.getHours()
 // Date.prototype.getMinutes()
 // Date.prototype.getSeconds()
